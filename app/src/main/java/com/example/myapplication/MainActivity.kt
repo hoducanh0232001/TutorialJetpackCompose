f62ffca2fb3b77339ff88c688291b3ce4a35f7e7
HoDucAnh <84118304+hoducanh0232001@users.noreply.github.com>
2023-07-03 22:02:29 +0700

    show content

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,17 +12,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,6 +34,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -88,7 +95,7 @@ private fun OnboardScreen(onContinueClicked: ()-> Unit, modifier: Modifier = Mod
 }
 @Composable
 private fun Greeting(name: String) {
-    var expanded by remember{mutableStateOf(false)}
+/*    var expanded by remember{mutableStateOf(false)}
     val extraPadding by animateDpAsState(if(expanded) 48.dp else 0.dp,
     animationSpec = spring(
         dampingRatio = Spring.DampingRatioMediumBouncy,
@@ -102,18 +109,64 @@ private fun Greeting(name: String) {
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "DucAnh,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.ExtraBold)
             }
             ElevatedButton(onClick = {
                 expanded = !expanded
             }) {
-                Text(if (expanded) "Show Less" else "Show More")
+                Text(if (expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more))
 
 
             }
         }
 
+    }*/
+    Card(colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary
+    ),
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
+        CardContent(name)
     }
+}
+@Composable
+private fun CardContent(name : String){
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    Row(modifier = Modifier
+        .padding(12.dp)
+        .animateContentSize(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if(expanded){
+                Text(text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4))
+            }
+        }
+        IconButton(onClick = {expanded = !expanded}) {
+            Icon(imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded){
+                    stringResource(R.string.show_less)
+                }else{
+                    stringResource(R.string.show_more)
+                })
+        }
+    }
+
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
@@ -122,7 +175,12 @@ fun GreetingPreview() {
     MyApplicationTheme {
       MyApp(Modifier.fillMaxSize())
     }
-}
+}@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun GreetingsPreview(){
